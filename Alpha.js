dd2d2fa46367ea749e7dcf8d67e04418e4f018e8
P-A-C-T-S <73199146+P-A-C-T-S@users.noreply.github.com>
2020-10-26 18:15:56 -0400

    Update Alpha.js

--- Alpha.js
+++ Alpha.js
@@ -1,2 +1,17 @@
-textSize(45);
-text("hello", 100, 100);
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title> P.A.C.T.S. </title>
+  </head>
+  
+ 
+  
+  <body>
+    PACTS sub-1
+    
+   
+</script>
+
+    
+  </body>
+  </html>
